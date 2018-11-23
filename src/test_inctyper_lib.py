c936e313573feafd36981cfcb8d056dda959d0ae
Corin Yeats <corin.yeats@gmail.com>
2018-11-23 09:23:16 +0000

    Some more unit testing to be sure.

--- src/test_inctyper_lib.py
+++ src/test_inctyper_lib.py
@@ -1,3 +1,4 @@
+import sys
 from unittest import TestCase
 
 import inctyper_lib as lib
@@ -8,14 +9,30 @@ class TestIncTyperLib(TestCase):
     def test_overlapping(self):
         match1 = {'qstart': 1, 'qend': 100}
         match2 = {'qstart': 75, 'qend': 200}
+        match3 = {'qstart': 82, 'qend': 150}
+        match4 = {'qstart': 100, 'qend': 200}
+
+        print("Checking normal overlap", file=sys.stderr)
         self.assertTrue(lib.overlapping(match1, match2, 20))
 
+        print("Checking overlap unaffected by argument order", file=sys.stderr)
         self.assertTrue(lib.overlapping(match2, match1, 20))
 
-        match3 = {'qstart': 82, 'qend': 150}
+        print("Checking not overlapping", file=sys.stderr)
         self.assertTrue(not lib.overlapping(match1, match3, 20))
 
+        print("Checking exactly identical matches are filtered", file=sys.stderr)
         self.assertTrue(lib.overlapping(match1, match1, 20))
+
+        print("Checking for off-by-one errors on overlaps (1)", file=sys.stderr)
+        self.assertTrue(lib.overlapping(match1, match4, 0))
+        self.assertTrue(lib.overlapping(match4, match1, 0))
+        self.assertTrue(lib.overlapping(match1, match2, 25))
+
+        print("Checking for off-by-one errors on overlaps (2)", file=sys.stderr)
+        self.assertTrue(not lib.overlapping(match1, match4, 1))
+        self.assertTrue(not lib.overlapping(match4, match1, 1))
+        self.assertTrue(not lib.overlapping(match1, match2, 26))
 
     def test_select_matches(self):
         match1 = {'qstart': 1, 'qend': 100, 'pid': 100.0}
