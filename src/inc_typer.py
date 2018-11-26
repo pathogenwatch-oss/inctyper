b6aaaff871d254bd045568d8fa901094b4cb5ff0
Corin Yeats <corin.yeats@gmail.com>
2018-11-26 09:16:22 +0000

    For humans.

--- src/inc_typer.py
+++ src/inc_typer.py
@@ -56,5 +56,5 @@ results = list()
 for contig in matches.keys():
     results.extend(sorted(lib.select_matches(matches[contig]), key=lambda match: match['Contig Start']))
 
-jsonResult = {'inc_matches': results}
+jsonResult = {'Inc Matches': results}
 print(json.dumps(jsonResult, indent=4), file=sys.stdout)
